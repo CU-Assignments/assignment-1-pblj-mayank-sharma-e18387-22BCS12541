ea89658fe786967f175792a74092ee6ab2efc617
Teesha <22bcs12541@cuchd.in>
2025-03-04 10:06:34 +0530

    Create StringAnalysis.java

---
+++ src/StringAnalysis.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class StringAnalysis {
+   public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        scanner.close();
+        int vowels = 0;
+        int consonants = 0;
+        int digits = 0;
+        int specialChars = 0;
+        input = input.toLowerCase(); 
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+
+            if (ch >= 'a' && ch <= 'z') {
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                    vowels++;
+                } else {
+                    consonants++;
+                }
+            } else if (ch >= '0' && ch <= '9') {
+                digits++;
+            } else {
+                if (ch != ' ') { 
+                    specialChars++;
+                }
+            }
+        }
+        System.out.println("Vowels: " + vowels);
+        System.out.println("Consonants: " + consonants);
+        System.out.println("Digits: " + digits);
+        System.out.println("Special Characters: " + specialChars);
+    }
+}
